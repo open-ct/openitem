2907774f7fe02dfcf51c1f100355f7a6c83e81ca
杨康 <3265047856@qq.com>
2022-07-23 22:00:48 +0800

    add base views

---
+++ web/src/views/Login/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import {getSigninUrl} from '../utils/setting'
+
+export default class login extends Component {
+//   constructor(){
+//     super()
+//     const res=getSigninUrl()
+//     window.location.href=res
+//   }
+  render() {
+    return (
+      <div>
+        <h4>即将跳转登录页面，请稍后。。。。。。</h4>
+      </div>
+    )
+  }
+}

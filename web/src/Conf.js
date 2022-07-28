3db18fb34d007dc6babefd9e2f351ab7e054b296
杨康 <3265047856@qq.com>
2022-07-28 22:26:46 +0800

    init dev config

--- web/src/Conf.js
+++ web/src/Conf.js
@@ -1,11 +1,17 @@
 export const AuthConfig = {
   // serverUrl: "https://door.casdoor.com",
+  // clientId: "eb78c88fc0697611f3e3",
+  // appName: "app-openitem",
+  // organizationName: "openct",
+  // redirectPath: "/callback",
+
   serverUrl: "http://localhost:7001",
-  clientId: "eb78c88fc0697611f3e3",
+  clientId : "55cb4ed35d67e678b55f",
   appName: "app-openitem",
-  organizationName: "openct",
+  organization : "openct",
   redirectPath: "/callback",
 };
+
 
 export const ForceLanguage = "";
 export const DefaultLanguage = "en";

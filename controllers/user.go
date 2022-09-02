de0168ec38133263f0664323d305e2179374860f
mktb <1270874562@qq.com>
2022-09-02 10:51:41 +0800

    all user list

--- controllers/user.go
+++ controllers/user.go
@@ -2,7 +2,16 @@ package controllers
 
 import "github.com/open-ct/openitem/casdoor"
 
+// GetUsers
+// @Title GetUsers
+// @Description 获取组织中的所有人员
+// @Success 200 {object} []*auth.User
+// @router /api/get-users [get]
 func (c *ApiController) GetUsers() {
+	if c.RequireSignedIn() {
+		return
+	}
+
 	c.Data["json"] = casdoor.GetUsers()
 	c.ServeJSON()
 }
